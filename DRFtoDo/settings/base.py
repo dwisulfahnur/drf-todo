68d625630c2ce27e5e6f42c623ee031d70fd906f
dwisulfahnur <dwisulfahnur@gmail.com>
2019-07-29 04:16:59 +0700

    Updatee the expired token time to 30minutes, cz 15s will kill your client 😭

--- DRFtoDo/settings/base.py
+++ DRFtoDo/settings/base.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT Auth Configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
