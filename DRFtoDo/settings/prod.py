96ef4fbe7c94366847be0b710f161ba11fd83a86
dwisulfahnur <dwisulfahnur@gmail.com>
2019-07-28 23:28:31 +0700

    Set default parser for production settings to json only

--- DRFtoDo/settings/prod.py
+++ DRFtoDo/settings/prod.py
@@ -4,7 +4,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -22,3 +21,5 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['rest_framework.renderers.JSONRenderer', ]
