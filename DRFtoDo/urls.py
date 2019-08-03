1081fb9bf30fdd7e58afa3ff6b0efd9bc653d664
dwisulfahnur <dwisulfahnur@gmail.com>
2019-08-04 00:31:05 +0700

    Add verify_token url on the url

--- DRFtoDo/urls.py
+++ DRFtoDo/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework_simplejwt.views import token_obtain_pair, token_refresh
+from rest_framework_simplejwt.views import token_obtain_pair, token_refresh, token_verify
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'api/v1/', include('todo.urls')),
     url(r'api/v1/obtain_token/', token_obtain_pair, name='token_obtain_pair'),
     url(r'api/v1/refresh_token/', token_refresh, name='token_refresh'),
+    url(r'api/v1/verify_token', token_verify, name='token_verify'),
 ]
