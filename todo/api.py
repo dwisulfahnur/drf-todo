14e262ecb98c88d61e3054297e5fb0e05f4b092d
dwisulfahnur <dwisulfahnur@gmail.com>
2019-08-04 00:29:55 +0700

    Add user field to the serializer and api of todo

--- todo/api.py
+++ todo/api.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import ToDoSerializer
@@ -10,9 +10,10 @@ class ToDoViewSet(viewsets.ModelViewSet):
     serializer_class = ToDoSerializer
     page_size_query_param = 'page_size'
     max_page_size = 100
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
-        return ToDo.objects.all()
+        return ToDo.objects.filter(user=self.request.user)
 
     @action(detail=True, methods=['post'])
     def set_as_complete(self, request, pk=None):
@@ -21,3 +22,6 @@ class ToDoViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(todo)
         return Response(serializer.data, status.HTTP_200_OK)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id)
--- todo/serializers.py
+++ todo/serializers.py
@@ -5,7 +5,8 @@ from .models import ToDo
 class ToDoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDo
-        fields = ('id', 'title', 'is_completed', 'completed_at', 'created_at', 'updated_at')
+        fields = ('id', 'user_id', 'title', 'is_completed', 'completed_at', 'created_at', 'updated_at')
         extra_kwargs = {
             'completed_at': {'read_only': True},
+            # 'user_id': {'read_only': True},
         }
